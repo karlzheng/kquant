9ceba959cc8fee09b2b1b36bf2e64752cbf412d2
KarlZheng <KarlZheng@linkcm.com>
2019-07-01 23:47:54 +0800

    add web/templates/compare.html
    Signed-off-by: KarlZheng <KarlZheng@linkcm.com>

--- web/demo.py
+++ web/demo.py
@@ -8,16 +8,35 @@ from pandas.compat import StringIO
 from pandas_highcharts.core import serialize
 
 app = Flask(__name__, static_url_path='')
-#app.secret_key = "2kQOLbr6NtfHV0wIItjHWzuwsgCUXA4CSSBWFE9yELqrkSZU"
 
 @app.route('/')
-def graph_Example(chartID = 'app', chart_type = 'line', chart_height = 500):
-    data = """Date,Value\n2019-12-31,1.7\n2018-12-31,3.7\n2017-12-31,1.7"""
-    df = pd.read_csv(StringIO(data), index_col=0, parse_dates=True)
-    df = df.sort_index()
-    dataset = serialize(df, render_to='mychart', secondary_y='frags', output_type='json')
+def compare(chartID = 'app', chart_type = 'line', chart_height = 500):
+    stock_code = "002713.SZ"
+    import tushare as ts
+    pro = ts.pro_api("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    import datetime
+    today = datetime.date.today()
+    threeyearsago = today - datetime.timedelta(days=365*3)
+    threeyearsagostr = threeyearsago.strftime('%Y%m%d')
+    todaystr = today.strftime('%Y%m%d')
 
-    return render_template('graph.html', chart=dataset)
+    shdf = pro.index_weekly(ts_code='000001.SH', start_date=threeyearsagostr, end_date=todaystr)
+    sh = shdf["close"].to_list()
+    sh.reverse()
+    maxsh = max(sh)
+
+    ssdf = pro.weekly(ts_code=stock_code, start_date=threeyearsagostr, end_date=todaystr)
+    ss = ssdf["close"].to_list()
+    ss.reverse()
+    maxss = max(ss)
+    ssnorm = []
+    for i in ss:
+        ssnorm.append(i * (maxsh) / maxss)
+    sh_str = sh.__str__()
+    ssnorm_str = ssnorm.__str__()
+
+    stock_code_str = "\'002713.SZ\'"
+    return render_template('compare.html', stock_code=stock_code_str, sh=sh_str, ss=ssnorm_str)
 
 if __name__ == "__main__":
     app.run(debug=True)
